b913fa317353326a472fe640b107a0fa3691bb97
铃 <2497158011@qq.com>
2021-07-30 16:51:07 +0800

    存取员工信息（hash)

--- rank-application/src/main/java/com/fei/rank/application/service/impl/RankServiceImpl.java
+++ rank-application/src/main/java/com/fei/rank/application/service/impl/RankServiceImpl.java
@@ -1,13 +1,11 @@
 package com.fei.rank.application.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
 
 import com.fei.rank.api.query.QueryTopTen;
 import com.fei.rank.application.dao.RankReportDao;
 import com.fei.rank.application.entity.EmployeeEntity;
 import com.fei.rank.application.redis.RedisService;
 import com.fei.rank.application.service.RankService;
-import com.fei.rank.application.support.FeiShuUserInfoView;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
